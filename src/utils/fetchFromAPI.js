b484d9f6ad3d69496326c32fce625bb3d803b287
SUBHAOO7 <subhajit18mondaloo7@gmail.com>
2023-12-16 21:45:51 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  method: 'GET',
+  url: 'https://youtube-v31.p.rapidapi.com/captions',
+  params: {
+    part: 'snippet',
+    videoId: 'M7FIvfx5J10'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'eeb908143bmsh5ea64b5884f08f9p12336bjsncc551a08be88',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
